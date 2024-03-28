be22c68ffcb5f17b6c8b98861db47224ec4bf778
IgoRM22 <i.ramos@aluno.ifsp.edu.br>
2024-03-28 20:49:43 -0300

    .

--- hello.py
+++ hello.py
@@ -1,32 +1,73 @@
+import os
 from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return '<User %r>' % self.username
 
-@app.route('/')
-def index():
-    users = User.query.all()
-    return render_template('index.html', users=users)
 
-@app.route('/add_user', methods=['GET', 'POST'])
-def add_user():
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/names')
+def names():
+    users = User.query.all()
+    return render_template('names.html', users=users)
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
     form = NameForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
@@ -34,8 +75,10 @@ def add_user():
             user = User(username=form.name.data)
             db.session.add(user)
             db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('add_user.html', form=form)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('names'))  # Redireciona para a página com a tabela de nomes
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False))
