c502047328d0cf67538e64e7bd2c73ffe9a3ece9
IgoRM22 <i.ramos@aluno.ifsp.edu.br>
2024-03-28 21:02:12 -0300

    a

--- hello.py
+++ hello.py
@@ -46,11 +46,6 @@ class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-class Pessoa(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(80), nullable=False)
-    idade = db.Column(db.Integer, nullable=False)
-
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
@@ -69,7 +64,6 @@ def internal_server_error(e):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
-    pessoas = Pessoa.query.all()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
@@ -81,5 +75,5 @@ def index():
             session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, pessoas=pessoas, name=session.get('name'),
+    return render_template('index.html', form=form, name=session.get('name'),
                            known=session.get('known', False))
